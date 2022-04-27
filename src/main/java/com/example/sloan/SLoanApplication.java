a92fd0b6934f90c2b772fbb116ef7947b1c6be83
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-04-27 23:06:31 +0100

    first commit

---
+++ src/main/java/com/example/sloan/SLoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.sloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SLoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SLoanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sloan/SLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SLoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
